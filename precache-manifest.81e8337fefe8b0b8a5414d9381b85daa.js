21e7c5f78894c5b1d4f163b014de989c46ace2d5
Cesare Moiola <cesaremoiola@gmail.com>
2022-04-09 12:08:43 +0200

    Updates

---
+++ precache-manifest.81e8337fefe8b0b8a5414d9381b85daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c152e9ff841fca5984892f6f208357",
+    "url": "/rosiko-fe/index.html"
+  },
+  {
+    "revision": "fe070a931362dad26dfd",
+    "url": "/rosiko-fe/static/css/main.1ed81eb5.chunk.css"
+  },
+  {
+    "revision": "ad6eb69ac21eff4e1b5e",
+    "url": "/rosiko-fe/static/js/2.b8714dd4.chunk.js"
+  },
+  {
+    "revision": "fb7738464dc3e89dc883",
+    "url": "/rosiko-fe/static/js/3.0e26a692.chunk.js"
+  },
+  {
+    "revision": "fe070a931362dad26dfd",
+    "url": "/rosiko-fe/static/js/main.5c339378.chunk.js"
+  },
+  {
+    "revision": "87fa217799034ce5f57c",
+    "url": "/rosiko-fe/static/js/runtime~main.f79cc1dc.js"
+  }
+]);
